2f6d2e1322e6dcbae08c644dd48cef7eb1492155
Ilyass Taouil <itaouil95@gmail.com>
2017-06-13 01:25:27 +0200

    Added Backend API along with database communication.

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ A Mongo, Express, Angular, Node TODO application. The technology stack is visibl
 2. Well Express is just Express 4.0.
 3. Angular is Angular 2.
 4. Node is version 8, so I am using/trying ES6.
+
+## **Development Process**
+
+1. **Basic Structure**
+
+I started by setting up the structure of the project, hence, creating our server (app.js), installing and requiring modules, setting up the middlewares, routes, view engine and static folders.
+
+Use the express generator, is faster !
+
+2. **Create routes/api endpoints + DB set up**
+
+We start by setting a mongodb on the clound using MLAB (free package), creating a user (for access control) as well as some documents to start with.
+
+The following step is to set up the routes of the API (database layer + backend) that our client (Angular) is going to consume during the interaction with the user. We set up the database connection by using the mongojs module for each route (add, update, get todos) we use the realative method offered by the module.
+
+3. **Set Up Client**
--- routes/todos.js
+++ routes/todos.js
@@ -2,9 +2,99 @@
 const express = require('express');
 const router  = express.Router();
 
+// Connect to database
+const mongojs = require('mongojs');
+const db      = mongojs('mongodb://dailand10:dailand10@ds123752.mlab.com:23752/meandtodosapp', ['todos']);
+
 // GET All Todos
 router.get('/todos', (req, res) => {
-  res.send('TODOS API');
+
+  // Find all documents in the collection
+  db.todos.find((err, todos) => {
+    if (err) res.status(400).send(err);
+    else res.json(todos);
+  });
+
+});
+
+// GET Single Todo
+router.get('/todo/:id', (req, res) => {
+
+  // Find a document using a native ObjectId
+  db.todos.findOne({
+    _id: mongojs.ObjectId(req.params.id)
+  }, (err, todo) => {
+    if (err) res.status(400).send(err);
+    else res.json(todo);
+  });
+
+});
+
+// Save Todo
+router.post('/todo', (req, res) => {
+
+  // Store todo received
+  var todo = req.body;
+
+  // Save a document in the collection
+  if (todo.text && todo.isCompleted) {
+    db.todos.save(todo, (err, result) => {
+      if (err) res.status(400).send(err);
+      else res.json(result);
+    });
+  }
+
+  // Invalid parameters passed
+  else {
+    res.status(400).json({
+        "error": "Invalid Data"
+    });
+  }
+
+});
+
+// Update Todo Document
+router.put('/todo/:id', (req, res) => {
+
+  var todo   = req.body;
+  var updObj = {}
+
+  if (todo.isCompleted) {
+    updObj.isCompleted = todo.isCompleted;
+  }
+
+  if (todo.text) {
+    updObj.text = todo.text;
+  }
+
+  // Check if object has been updated
+  if (!updObj) {
+    res.status(400).json({
+      "error": "Invalid Data"
+    });
+  }
+
+  else {
+    db.todos.update({
+      _id: mongojs.ObjectId(req.params.id)
+    }, updObj, {}, (err, result) => {
+      if (err) res.send(err);
+      else res.json(result);
+    });
+  }
+
+});
+
+// Delete Todo
+router.delete('/todo/:id', (req, res) => {
+
+  db.todos.remove({
+    _id: mongojs.ObjectId(req.params.id)
+  }, '', (err, result) => {
+    if (err) res.send(err);
+    else res.json(result);
+  });
+
 });
 
 // Export router
