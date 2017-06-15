fc3c176326b85d5ced9872ca9e0c2a9817e2116a
Ilyass Taouil <itaouil95@gmail.com>
2017-06-15 12:36:08 +0200

    Added UI components.

--- client/src/app/components/todos.component.js
+++ client/src/app/components/todos.component.js
@@ -6,17 +6,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
-var AppComponent = (function () {
-    function AppComponent() {
+var TodosComponent = (function () {
+    function TodosComponent() {
     }
-    return AppComponent;
+    return TodosComponent;
 }());
-AppComponent = __decorate([
+TodosComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
-        selector: 'my-app',
-        templateUrl: 'app.component.html',
+        selector: 'todos',
+        templateUrl: 'todos.component.html',
     })
-], AppComponent);
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
+], TodosComponent);
+exports.TodosComponent = TodosComponent;
+//# sourceMappingURL=todos.component.js.map
